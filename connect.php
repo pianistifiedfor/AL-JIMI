972105bec0dbd7f2abdef01702fd37ef43b3bc41
pianistifiedfor <84398523+pianistifiedfor@users.noreply.github.com>
2021-09-09 05:54:52 -0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+mysql_connect("localhost","root","") or die (mysql_error());
+mysql_select_db("data") or die (mysql_error());
+
+
+?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+// Any mobile device (phones or tablets).
+if ( $detect->isMobile() ) {
+echo("You are on a mobile device, please visit aljimicurtains.ml for the mobile including tablet version of this website...")
+}
+
+?>
